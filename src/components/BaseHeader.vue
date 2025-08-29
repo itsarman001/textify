1d25519142203c57492d3685bb3e85c716cd3e96
Md Arman <armanar6712@gmail.com>
2025-08-29 18:03:17 +0530

    Push all the changes

---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import LayoutList from "./icons/LayoutList.vue";
+</script>
+
+<template>
+  <header>
+    <nav class="flex justify-between items-center w-full">
+      <h2>Welcome back, Arman</h2>
+
+      <div class="ms-auto flex gap-2">
+        <button class="flex items-center gap-2 text-white px-4 py-2 rounded">
+          <LayoutList color="white" />
+          <span>Customize</span>
+        </button>
+      </div>
+    </nav>
+  </header>
+</template>
---
+++ src/pages/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import BaseHeader from "../components/BaseHeader.vue";
+</script>
+
+<template>
+  <div class="relative">
+    <img
+      class="h-screen w-full absolute top-0 left-0 z-0"
+      src="/sunset.jpg"
+      alt="background"
+    />
+    <div
+      class="h-screen p-8 absolute z-10 top-0 left-0 bg-transparent text-slate-400"
+    >
+      <BaseHeader />
+    </div>
+  </div>
+</template>
